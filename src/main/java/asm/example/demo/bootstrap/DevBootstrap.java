c0b6d26bd581e3caec8fff80c183286905ee4b13
michael-mcmasters <jmcmasters411@gmail.com>
2021-02-01 21:27:41 -0500

    Create bootstrap package. Add new student to server

--- src/main/java/asm/example/demo/bootstrap/DevBootstrap.java
+++ src/main/java/asm/example/demo/bootstrap/DevBootstrap.java
@@ -1,4 +1,4 @@
-package asm.example.demo.initializedata;
+package asm.example.demo.bootstrap;
 
 import asm.example.demo.domain.Student;
 import asm.example.demo.repositories.StudentRepo;
@@ -6,11 +6,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Bootstrap implements CommandLineRunner {
+public class DevBootstrap implements CommandLineRunner {
 
     private final StudentRepo studentRepo;
 
-    public Bootstrap(StudentRepo studentRepo) {
+    public DevBootstrap(StudentRepo studentRepo) {
         this.studentRepo = studentRepo;
     }
 
@@ -20,7 +20,13 @@ public class Bootstrap implements CommandLineRunner {
         student.setFirstName("Peter");
         student.setLastName("Peterson");
 
+        Student student2 = new Student();
+        student2.setFirstName("Ami");
+        student2.setLastName("Amison");
+
         studentRepo.save(student);
+        studentRepo.save(student2);
         System.out.println(student);
+        System.out.println(student2);
     }
 }
