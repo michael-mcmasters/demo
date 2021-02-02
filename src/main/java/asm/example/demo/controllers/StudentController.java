b51796f5ede3807db2ed98733f26b49b56f02804
michael-mcmasters <jmcmasters411@gmail.com>
2021-02-01 20:03:23 -0500

    Return JSON instead of HTML

--- src/main/java/asm/example/demo/controllers/StudentController.java
+++ src/main/java/asm/example/demo/controllers/StudentController.java
@@ -1,30 +1,35 @@
 package asm.example.demo.controllers;
 
+import asm.example.demo.domain.Student;
 import asm.example.demo.repositories.StudentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller         // Doesn't work if we use @RestController
+import java.util.Optional;
+
+@RestController         // Doesn't work if we use @RestController
 public class StudentController {
 
     private final StudentRepo studentRepo;
 
+    @Autowired
     public StudentController(StudentRepo studentRepo) {
         this.studentRepo = studentRepo;
     }
 
     @RequestMapping("")
-    public String getStudent(Model model) {
-        model.addAttribute("student", studentRepo.findAll());
-
-        return "AllStudents";
+    public Student getStudent() {
+        Optional<Student> student = studentRepo.findByFirstName("Peter");
+        return student.get();
     }
 
-    @RequestMapping("/student/first-name")
-    public String getStudentFirstName(Model model) {
-        model.addAttribute("student", studentRepo.findAll());
-
-        return "AllStudentsFirstName";
+    @RequestMapping("/student/{firstname}")
+    public Student getStudentFirstName(@PathVariable String firstname) {
+        Optional<Student> student = studentRepo.findByFirstName(firstname);
+        return student.get();
     }
 }
--- src/main/java/asm/example/demo/repositories/StudentRepo.java
+++ src/main/java/asm/example/demo/repositories/StudentRepo.java
@@ -3,6 +3,8 @@ package asm.example.demo.repositories;
 import asm.example.demo.domain.Student;
 import org.springframework.data.repository.CrudRepository;
 
-public interface StudentRepo extends CrudRepository<Student, Long> {
+import java.util.Optional;
 
+public interface StudentRepo extends CrudRepository<Student, Long> {
+    Optional<Student> findByFirstName(String name);
 }
